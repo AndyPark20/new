4d47f5097580a8a7e659a2101eb3db11e181d482
AndyPark20 <apark1214@gmail.com>
2022-09-09 07:21:07 -0700

    React practice'

--- my-app/src/test.js
+++ my-app/src/test.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 
 
 
 export const Test =()=>{
+
+    const [numbers, updateNumbers] = useState(0)
+
     return (
         <div>
             <h3>Hello world from inner component</h3>
